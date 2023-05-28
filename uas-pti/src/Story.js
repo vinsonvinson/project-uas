9a87bf48e0c4f1b540589250d44530b043bd3069
SketPlayer <riohu137@gmail.com>
2023-05-29 00:44:17 +0700

    Add Story Style

--- uas-pti/src/Story.js
+++ uas-pti/src/Story.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Typewriter from "typewriter-effect";
 import './Story.css';
@@ -8,6 +8,8 @@ function Story() {
   const location = useLocation();
   const { name } = location.state || {};
   const [isComplete, setIsComplete] = useState(false);
+  const [isComplete2, setIsComplete2] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
   const handleClick = () => {
       navigate('/quiz');
@@ -17,13 +19,24 @@ function Story() {
     setIsComplete(true);
   };
 
+  const handleComplete2 = () => {
+    setIsComplete2(true);
+  };
+
+  useEffect(() => {
+    if (isComplete2) {
+      setShowButton(true);
+    }
+  }, [isComplete2]);
+
   return (
     <div className="story-container">
-      <h1 className='storytitle'>Story Page</h1>
+      <h1 className='storytitle'>Fantastic Nusantara</h1>
       <div className="paper">
       <Typewriter
         onInit={(typewriter) => {
           typewriter
+            .pauseFor(720)
             .typeString(
               "This story is about " +name+ ", a hero from the land of Nusantara. " +name+ " is a smart yet brave hero that will bring peace to Nusantara. "
               +"However, " +name+ " will face some tough challenges ahead that will test the hero's knowledge and determination. Monsters from the depths of darkness have come to purge the land of Nusantara."
@@ -44,8 +57,10 @@ function Story() {
         onInit={(typewriter) => {
           typewriter
             .typeString(
-              "Will " +name+ " be able to achieve victory and save the land of Nusantara? Only you can know the answer."
+              "<span style='font-weight: bold;'>Will " +name+ " be able to achieve victory and save the land of Nusantara? Only you can know the answer.</span>"
             )
+            .pauseFor(1000)
+            .callFunction(handleComplete2)
             .start();
         }}
         options={{
@@ -55,7 +70,9 @@ function Story() {
       />
       )}
       </div>
-      <button className="continueBtn" onClick={handleClick}>Continue</button>
+      {showButton && (
+        <button className="continueBtn" onClick={handleClick}>Continue</button>
+      )}
     </div>
   );
 }
